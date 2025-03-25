4a310d68ba3f1aa82c9896a169a5b6282263fa18
Aden Margheriti <adennn@gmail.com>
2025-03-25 16:43:46 +1100

    remove unneeded file

--- src/types.d.ts
+++
@@ -1,7 +0,0 @@
-// Type declarations for browser WebExtension APIs
-// This is a simplified version to allow the extension to compile correctly
-
-declare namespace browser {
-  // Add any browser APIs you need to use here
-  // For now we're keeping it minimal since we're only logging to console
-}
